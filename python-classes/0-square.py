47a5263ce0cf6fcb5c51d0ed5a12ef249d5120f0
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-13 23:12:34 +0000

    Task 0 completed: Square class with private size

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square by its size.
+    Attributes:
+        __size (int): The size of the square (private)
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
